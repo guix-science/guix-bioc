00651467d07dd09017068781e1d70603cc06c609
User for Guix Bioconductor updates <guix-bioc@berlin.guix.gnu.org>
2025-01-13 09:54:56 +0100

    Automated CRAN import 2025-01-13
    Compatible with the following channels:
    
    (list (channel
            (name 'guix-cran)
            (url "https://github.com/guix-science/guix-cran.git")
            (branch "master")
            (commit "8a9caae1fee95ef9e20ab5fd34fe1ca6c0b8efbf"))
          (channel
            (name 'guix)
            (url "https://git.savannah.gnu.org/git/guix.git")
            (branch "master")
            (commit "4fef7b26941940515533b4b463b505b28567afe6")
            (introduction
             (make-channel-introduction "9edb3f66fd807b096b48283debdcddccfea34bad"
              (openpgp-fingerprint
               "BBB0 2DDF 2CEA F6A8 0D1D  E643 A2A0 6DF2 A33A 54FA")))))

--- guix-bioc/packages/g.scm
+++ guix-bioc/packages/g.scm
@@ -19,6 +19,7 @@
   #:use-module (gnu packages compression)
   #:use-module (guix-cran packages p)
   #:use-module (guix-cran packages t)
+  #:use-module (gnu packages bioinformatics)
   #:use-module (guix-cran packages h)
   #:use-module (guix-cran packages r)
   #:use-module (guix-cran packages l)
